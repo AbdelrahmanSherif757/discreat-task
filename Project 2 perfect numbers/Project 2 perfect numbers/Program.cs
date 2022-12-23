7fd45466e8fd3c3c56b33ea553b8595e8b430d64
AbdelrahmanSherif757 <121310025+AbdelrahmanSherif757@users.noreply.github.com>
2022-12-23 19:31:55 +0200

    Add files via upload

---
+++ Project 2 perfect numbers/Project 2 perfect numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+public class Exercise28
+{
+    public static void Main()
+    {
+        int n, i, sum;
+        int mn, mx;
+
+        Console.WriteLine("find perfect numbers");
+
+        Console.Write(" enter the frist number: ");
+        int fristnumber = int.Parse(Console.ReadLine());
+
+        Console.Write("enter the last number : ");
+        int lastnumber = int.Parse(Console.ReadLine());
+
+
+
+        for (n = fristnumber; n <= lastnumber; n++)
+        {
+            i = 1;
+            sum = 0;
+            while (i < n)
+            {
+                if (n % i == 0)
+                    sum = sum + i;
+                i++;
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("the perfect numbers :");
+                Console.WriteLine("{0} ", n);
+
+            }
+
+
+        }
+        Console.Write("\n");
+    }
+}
---
+++ project 1 prime numbers/project 1 prime numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿int T = 0;
+Console.WriteLine("ENTER THE FRIST NUMBER ");
+int N1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("ENTER THE SECOND NUMBER ");
+int N2 = int.Parse(Console.ReadLine());
+
+
+for (int i = N1; i <= N2; i++)
+{
+    for (int c = 1; c <= i; c++)
+    {
+        if (i % c == 0)
+        {
+            T++;
+        }
+    }
+
+    if (T == 2)
+    {
+        double perfect1 = Math.Pow(2, i) - 1;
+        double perfect2 = Math.Pow(2, i - 1);
+        Console.WriteLine("the perfect numbers is  " + perfect1*perfect2);
+        T = 0;
+
+    }
+    else
+    {
+        T = 0;
+    }
+
+}
